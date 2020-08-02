c386644cb2c41c10adb138f04faeeb2872c3cd20
bohrzhang <bohrzhang@tencent.com>
2020-08-02 13:58:00 +0800

    chrome加载插件开发

--- assets/popup.js
+++ assets/popup.js
@@ -1,15 +1,33 @@
 const rcBtn = document.getElementById('record')
 const loadBtn = document.getElementById('load')
+const addBtn = document.getElementById('add-btn')
+const form = document.getElementById('form')
 
+let content = {
+  host: '',
+  query: ''
+}
 rcBtn.addEventListener('click', function(e) {
   chrome.tabs.query({active: true}, function(tabs) {
     const currentPage = tabs[0]
     const url = currentPage.url
-    console.log(url)
+    const [host, query] = url.split('?')
+    content = {host, query}
   })
 })
 
 loadBtn.addEventListener('click', function(e) {
   const optionPage = chrome.runtime.getURL('options.html')
   window.open(optionPage)
+})
+
+addBtn.addEventListener('click', function() {
+  const hostName = form['hostName'].value
+  const paramName = form['paramName'].value
+  chrome.storage.sync.get(['customHostCache'], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
+  // chrome.storage.sync.set({key: value}, function() {
+  //   console.log('Value is set to ' + value);
+  // });
 })
