6aa28d84ea0c6098699a23ce4e0b79a6e2f07892
bohrzhang <bohrzhang@tencent.com>
2020-08-18 21:39:40 +0800

    修改链接获取问题

--- assets/popup.js
+++ assets/popup.js
@@ -1,20 +1,6 @@
-const rcBtn = document.getElementById('record')
-const loadBtn = document.getElementById('load')
+const loadBtn = document.getElementById('load-btn')
 const addBtn = document.getElementById('add-btn')
 const form = document.getElementById('form')
-
-let content = {
-  host: '',
-  query: ''
-}
-rcBtn.addEventListener('click', function(e) {
-  chrome.tabs.query({active: true}, function(tabs) {
-    const currentPage = tabs[0]
-    const url = currentPage.url
-    const [host, query] = url.split('?')
-    content = {host, query}
-  })
-})
 
 loadBtn.addEventListener('click', function(e) {
   const optionPage = chrome.runtime.getURL('options.html')
@@ -22,11 +8,22 @@ loadBtn.addEventListener('click', function(e) {
 })
 
 addBtn.addEventListener('click', function() {
-  const hostName = form['hostName'].value
-  const paramName = form['paramName'].value
-  chrome.storage.sync.get(['customHostCache'], function(result) {
+
+  const hostName = form['hostValue'].value
+  const pathName = form['pathValue'].value
+  const queryName = form['queryValue'].value
+  chrome.tabs.query({active: true}, function(tabs) {
+    const currentPage = tabs[0]
+    console.log(currentPage, currentPage.url)
+    const url = currentPage.url
+    const [host, query] = url.split('?')
+    const content = {host, query}
+  })
+  
+  chrome.storage.sync.get(['wukongCache'], function(result) {
     console.log('Value currently is ' + result.key);
   });
+
   // chrome.storage.sync.set({key: value}, function() {
   //   console.log('Value is set to ' + value);
   // });
