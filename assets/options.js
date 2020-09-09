5452dc05ce3ee8d0fef7f65c05712a1b86ac3171
bohrzhang <bohrzhang@tencent.com>
2020-09-09 12:49:41 +0800

    添加lint规范

--- assets/options.js
+++ assets/options.js
@@ -1,13 +1,13 @@
-let page = document.getElementById('buttonDiv');
+const page = document.getElementById('buttonDiv');
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
+  for (const item of kButtonColors) {
+    const button = document.createElement('button');
     button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
+    button.addEventListener('click', () => {
+      chrome.storage.sync.set({ color: item }, () => {
+        console.log(`color is ${item}`);
+      });
     });
     page.appendChild(button);
   }
--- assets/popup.js
+++ assets/popup.js
@@ -1,30 +1,30 @@
-const loadBtn = document.getElementById('load-btn')
-const addBtn = document.getElementById('add-btn')
-const form = document.getElementById('form')
+/* eslint-disable no-unused-vars */
+const loadBtn = document.getElementById('load-btn');
+const addBtn = document.getElementById('add-btn');
+const form = document.getElementById('form');
 
-loadBtn.addEventListener('click', function(e) {
-  const optionPage = chrome.runtime.getURL('options.html')
-  window.open(optionPage)
-})
+loadBtn.addEventListener('click', () => {
+  const optionPage = chrome.runtime.getURL('options.html');
+  window.open(optionPage);
+});
 
-addBtn.addEventListener('click', function() {
+addBtn.addEventListener('click', () => {
+  const hostName = form.hostValue.value;
+  const pathName = form.pathValue.value;
+  const queryName = form.queryValue.value;
+  chrome.tabs.query({ active: true }, (tabs) => {
+    const [currentPage] = tabs;
+    console.log(currentPage, currentPage.url);
+    const { url } = currentPage;
+    const [host, query] = url.split('?');
+    const content = { host, query };
+  });
 
-  const hostName = form['hostValue'].value
-  const pathName = form['pathValue'].value
-  const queryName = form['queryValue'].value
-  chrome.tabs.query({active: true}, function(tabs) {
-    const currentPage = tabs[0]
-    console.log(currentPage, currentPage.url)
-    const url = currentPage.url
-    const [host, query] = url.split('?')
-    const content = {host, query}
-  })
-  
-  chrome.storage.sync.get(['wukongCache'], function(result) {
-    console.log('Value currently is ' + result.key);
+  chrome.storage.sync.get(['wukongCache'], (result) => {
+    console.log(`Value currently is ${result.key}`);
   });
 
   // chrome.storage.sync.set({key: value}, function() {
   //   console.log('Value is set to ' + value);
   // });
-})
+});
