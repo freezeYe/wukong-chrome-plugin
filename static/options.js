235ddb893384b4dbf3b277a4ee0ec2907621fdd6
bohrzhang <bohrzhang@tencent.com>
2020-09-27 20:49:07 +0800

    feat: 插件列表页开发完成

--- static/options.js
+++ static/options.js
@@ -1,24 +1,24 @@
-const hostContainer = document.getElementById('host')
-const pathContainer = document.getElementById('path')
-const queryContainer = document.getElementById('query')
+const hostContainer = document.getElementById('host');
+const pathContainer = document.getElementById('path');
+const queryContainer = document.getElementById('query');
 
 chrome.storage.sync.get(['__wukongCache'], (result) => {
-  const {__wukongCache = {}} = result
-  const {hostMap, pathMap, queryMap} = __wukongCache
-  let hostHtml = ''
-  let pathHtml = ''
-  let queryHtml = ''
-  Object.keys(hostMap).forEach(hostKey => {
-    hostHtml += `<li>${hostMap[hostKey]}: ${hostKey}</li>`
-  })
-  Object.keys(pathMap).forEach(pathKey => {
-    pathHtml += `<li>${pathMap[pathKey]}: ${pathKey}</li>`
-  })
-  Object.keys(queryMap).forEach(querykey => {
-    queryHtml += `<li>${queryMap[querykey]}: ${querykey}</li>`
-  })
+  const { __wukongCache = {} } = result;
+  const { hostMap, pathMap, queryMap } = __wukongCache;
+  let hostHtml = '';
+  let pathHtml = '';
+  let queryHtml = '';
+  Object.keys(hostMap).forEach((hostKey) => {
+    hostHtml += `<li>${hostMap[hostKey]}: ${hostKey}</li>`;
+  });
+  Object.keys(pathMap).forEach((pathKey) => {
+    pathHtml += `<li>${pathMap[pathKey]}: ${pathKey}</li>`;
+  });
+  Object.keys(queryMap).forEach((querykey) => {
+    queryHtml += `<li>${queryMap[querykey]}: ${querykey}</li>`;
+  });
 
-  hostContainer.innerHTML = hostHtml
-  pathContainer.innerHTML = pathHtml
-  queryContainer.innerHTML = queryHtml
-})
+  hostContainer.innerHTML = hostHtml;
+  pathContainer.innerHTML = pathHtml;
+  queryContainer.innerHTML = queryHtml;
+});
--- static/popup.js
+++ static/popup.js
@@ -15,120 +15,120 @@ const init = () => {
     link = url;
     linkp.innerText = url;
   });
-  initInSavePage(link)
+  initInSavePage(link);
 };
 
 // 保存链接
 function saveLink(link) {
-    // 命名字段
-    const hostName = form.hostValue.value;
-    const pathName = form.pathValue.value;
-    const queryName = form.queryValue.value;
-    if (!hostName && !pathName && !queryName) {
-      reminder.innerText = '参数不能全部为空!';
-      reminder.style.color = 'red';
-      return;
-    }
-    // 链接字段值
-    const [href, query = ''] = link.split('?');
-    const args = href.split('/');
-    const host = args.slice(0, 3).join('/');
-    const path = args.slice(3).join('/');
-    // 存储key
-    const key = '__wukongCache';
-    chrome.storage.sync.get([key], (result) => {
-      const json = result[key] || {};
-      const { hostMap = {}, pathMap = {}, queryMap = {} } = json;
-      if (hostName) hostMap[host] = hostName;
-      if (pathName) pathMap[path] = pathName;
-      if (queryName) queryMap[query] = queryName;
+  // 命名字段
+  const hostName = form.hostValue.value;
+  const pathName = form.pathValue.value;
+  const queryName = form.queryValue.value;
+  if (!hostName && !pathName && !queryName) {
+    reminder.innerText = '参数不能全部为空!';
+    reminder.style.color = 'red';
+    return;
+  }
+  // 链接字段值
+  const [href, query = ''] = link.split('?');
+  const args = href.split('/');
+  const host = args.slice(0, 3).join('/');
+  const path = args.slice(3).join('/');
+  // 存储key
+  const key = '__wukongCache';
+  chrome.storage.sync.get([key], (result) => {
+    const json = result[key] || {};
+    const { hostMap = {}, pathMap = {}, queryMap = {} } = json;
+    if (hostName) hostMap[host] = hostName;
+    if (pathName) pathMap[path] = pathName;
+    if (queryName) queryMap[query] = queryName;
 
-      json.hostMap = hostMap;
-      json.pathMap = pathMap;
-      json.queryMap = queryMap;
-      chrome.storage.sync.set({ [key]: json });
-      reminder.innerText = '保存成功!';
-      reminder.style.color = 'green';
-      setTimeout(() => {
-        reminder.innerText = '';
-      }, 2000);
-    });
+    json.hostMap = hostMap;
+    json.pathMap = pathMap;
+    json.queryMap = queryMap;
+    chrome.storage.sync.set({ [key]: json });
+    reminder.innerText = '保存成功!';
+    reminder.style.color = 'green';
+    setTimeout(() => {
+      reminder.innerText = '';
+    }, 2000);
+  });
 }
 
 // 加载记录列表
 function loadStored() {
-  let count = 0
-  const childrenWrapper = (key ,value, name)=> {
-    const id = name + count
-    count += 1
-    return `<li><input type="radio" id=${id} name=${name} value=${key}><label for=${id}>${value}</label></input></li>`
-  }
+  let count = 0;
+  const childrenWrapper = (key, value, name) => {
+    const id = name + count;
+    count += 1;
+    return `<li><input type="radio" id=${id} name=${name} value=${key}><label for=${id}>${value}</label></input></li>`;
+  };
   chrome.storage.sync.get(['__wukongCache'], (result) => {
-    const hostContainer = document.getElementById('host-list')
-    const pathContainer = document.getElementById('path-list')
-    const queryContainer = document.getElementById('query-list')
-    const {__wukongCache = {}} = result
-    const {hostMap, pathMap, queryMap} = __wukongCache
-    let hostHtml = ''
-    let pathHtml = ''
-    let queryHtml = ''
-    Object.keys(hostMap).forEach(hostKey => {
-      hostHtml += childrenWrapper(hostKey, hostMap[hostKey], 'hostValue')
-    })
-    Object.keys(pathMap).forEach(pathKey => {
-      pathHtml += childrenWrapper(pathKey, pathMap[pathKey], 'pathValue')
-    })
-    Object.keys(queryMap).forEach(querykey => {
-      queryHtml += childrenWrapper(querykey, queryMap[querykey], 'queryValue')
-    })
-    hostContainer.innerHTML = `<li class="header">域名</li>` + hostHtml
-    pathContainer.innerHTML = `<li class="header">路径</li>` + pathHtml
-    queryContainer.innerHTML = `<li class="header">参数</li>` + queryHtml
+    const hostContainer = document.getElementById('host-list');
+    const pathContainer = document.getElementById('path-list');
+    const queryContainer = document.getElementById('query-list');
+    const { __wukongCache = {} } = result;
+    const { hostMap, pathMap, queryMap } = __wukongCache;
+    let hostHtml = '';
+    let pathHtml = '';
+    let queryHtml = '';
+    Object.keys(hostMap).forEach((hostKey) => {
+      hostHtml += childrenWrapper(hostKey, hostMap[hostKey], 'hostValue');
+    });
+    Object.keys(pathMap).forEach((pathKey) => {
+      pathHtml += childrenWrapper(pathKey, pathMap[pathKey], 'pathValue');
+    });
+    Object.keys(queryMap).forEach((querykey) => {
+      queryHtml += childrenWrapper(querykey, queryMap[querykey], 'queryValue');
+    });
+    hostContainer.innerHTML = `<li class="header">域名</li>${hostHtml}`;
+    pathContainer.innerHTML = `<li class="header">路径</li>${pathHtml}`;
+    queryContainer.innerHTML = `<li class="header">参数</li>${queryHtml}`;
 
-    initInListPage()
-  })
+    initInListPage();
+  });
 }
 
 // 页面加载
 function loadPage() {
-  const getCheckdValue = name=> {
+  const getCheckdValue = (name) => {
     const radios = document.getElementsByName(name);
-    for (let i =0; i<radios.length; i++) {
-      if (radios[i].checked) return radios[i].value
+    for (let i = 0; i < radios.length; i++) {
+      if (radios[i].checked) return radios[i].value;
     }
-  }
-  const hostValue = getCheckdValue('hostValue')
-  const pathValue = getCheckdValue('pathValue')
-  const queryValue = getCheckdValue('queryValue')
+  };
+  const hostValue = getCheckdValue('hostValue');
+  const pathValue = getCheckdValue('pathValue');
+  const queryValue = getCheckdValue('queryValue');
   if (!hostValue) {
     reminder.style.color = 'red';
-    reminder.innerText = "请选择域名!"
-    return
+    reminder.innerText = '请选择域名!';
+    return;
   }
-  let link = hostValue
-  if (pathValue) link += `/${pathValue}`
-  if (queryValue) link += `?${queryValue}`
-  window.open(link)
+  let link = hostValue;
+  if (pathValue) link += `/${pathValue}`;
+  if (queryValue) link += `?${queryValue}`;
+  window.open(link);
 }
 
 // 编辑页事件绑定
 function initInSavePage() {
-  reminder.innerText = ""
-  addBtn.innerText = "保存"
-  form.style.display = 'block'
-  loadContainer.style.display = "none"
-  loadBtn.onclick = loadStored
-  addBtn.onclick = saveLink
+  reminder.innerText = '';
+  addBtn.innerText = '保存';
+  form.style.display = 'block';
+  loadContainer.style.display = 'none';
+  loadBtn.onclick = loadStored;
+  addBtn.onclick = saveLink;
 }
 
 // 列表页事件绑定
 function initInListPage() {
-  reminder.innerText = ""
-  addBtn.innerText = "返回"
-  form.style.display = 'none'
-  loadContainer.style.display = "flex"
-  loadBtn.onclick = loadPage
-  addBtn.onclick = initInSavePage
+  reminder.innerText = '';
+  addBtn.innerText = '返回';
+  form.style.display = 'none';
+  loadContainer.style.display = 'flex';
+  loadBtn.onclick = loadPage;
+  addBtn.onclick = initInSavePage;
 }
 
 init();
