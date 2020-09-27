839921d7e749194a3822c8c933d6e1e47af32d5f
bohrzhang <bohrzhang@tencent.com>
2020-09-27 20:47:11 +0800

    feat: 插件列表页开发完成

--- static/popup.js
+++ static/popup.js
@@ -6,37 +6,34 @@ const linkp = document.getElementById('link-show');
 const reminder = document.getElementById('reminder');
 const loadContainer = document.getElementById('load-container');
 
+let link = '';
 // 项目初始化，绑定相关事件
 const init = () => {
-  let link = '';
   chrome.tabs.query({ active: true }, (tabs) => {
     const [currentPage] = tabs;
-    console.log(currentPage, currentPage.url);
     const { url } = currentPage;
     link = url;
     linkp.innerText = url;
   });
+  initInSavePage(link)
+};
 
-  loadBtn.addEventListener('click', loadStored);
-  addBtn.addEventListener('click', () => {
+// 保存链接
+function saveLink(link) {
     // 命名字段
     const hostName = form.hostValue.value;
     const pathName = form.pathValue.value;
     const queryName = form.queryValue.value;
-
     if (!hostName && !pathName && !queryName) {
       reminder.innerText = '参数不能全部为空!';
-      reminder.style.display = 'block';
       reminder.style.color = 'red';
       return;
     }
-
     // 链接字段值
     const [href, query = ''] = link.split('?');
     const args = href.split('/');
     const host = args.slice(0, 3).join('/');
     const path = args.slice(3).join('/');
-
     // 存储key
     const key = '__wukongCache';
     chrome.storage.sync.get([key], (result) => {
@@ -52,16 +49,20 @@ const init = () => {
       chrome.storage.sync.set({ [key]: json });
       reminder.innerText = '保存成功!';
       reminder.style.color = 'green';
-      reminder.style.display = 'block';
       setTimeout(() => {
-        reminder.style.display = 'none';
+        reminder.innerText = '';
       }, 2000);
     });
-  });
-};
+}
 
 // 加载记录列表
 function loadStored() {
+  let count = 0
+  const childrenWrapper = (key ,value, name)=> {
+    const id = name + count
+    count += 1
+    return `<li><input type="radio" id=${id} name=${name} value=${key}><label for=${id}>${value}</label></input></li>`
+  }
   chrome.storage.sync.get(['__wukongCache'], (result) => {
     const hostContainer = document.getElementById('host-list')
     const pathContainer = document.getElementById('path-list')
@@ -72,21 +73,62 @@ function loadStored() {
     let pathHtml = ''
     let queryHtml = ''
     Object.keys(hostMap).forEach(hostKey => {
-      hostHtml += `<li>${hostMap[hostKey]}</li>`
+      hostHtml += childrenWrapper(hostKey, hostMap[hostKey], 'hostValue')
     })
     Object.keys(pathMap).forEach(pathKey => {
-      pathHtml += `<li>${pathMap[pathKey]}</li>`
+      pathHtml += childrenWrapper(pathKey, pathMap[pathKey], 'pathValue')
     })
     Object.keys(queryMap).forEach(querykey => {
-      queryHtml += `<li>${queryMap[querykey]}</li>`
+      queryHtml += childrenWrapper(querykey, queryMap[querykey], 'queryValue')
     })
-    hostContainer.innerHTML = hostHtml
-    pathContainer.innerHTML = pathHtml
-    queryContainer.innerHTML = queryHtml
+    hostContainer.innerHTML = `<li class="header">域名</li>` + hostHtml
+    pathContainer.innerHTML = `<li class="header">路径</li>` + pathHtml
+    queryContainer.innerHTML = `<li class="header">参数</li>` + queryHtml
 
-    form.style.display = 'none'
-    loadContainer.style.display = "flex"
+    initInListPage()
   })
+}
+
+// 页面加载
+function loadPage() {
+  const getCheckdValue = name=> {
+    const radios = document.getElementsByName(name);
+    for (let i =0; i<radios.length; i++) {
+      if (radios[i].checked) return radios[i].value
+    }
+  }
+  const hostValue = getCheckdValue('hostValue')
+  const pathValue = getCheckdValue('pathValue')
+  const queryValue = getCheckdValue('queryValue')
+  if (!hostValue) {
+    reminder.style.color = 'red';
+    reminder.innerText = "请选择域名!"
+    return
+  }
+  let link = hostValue
+  if (pathValue) link += `/${pathValue}`
+  if (queryValue) link += `?${queryValue}`
+  window.open(link)
+}
+
+// 编辑页事件绑定
+function initInSavePage() {
+  reminder.innerText = ""
+  addBtn.innerText = "保存"
+  form.style.display = 'block'
+  loadContainer.style.display = "none"
+  loadBtn.onclick = loadStored
+  addBtn.onclick = saveLink
+}
+
+// 列表页事件绑定
+function initInListPage() {
+  reminder.innerText = ""
+  addBtn.innerText = "返回"
+  form.style.display = 'none'
+  loadContainer.style.display = "flex"
+  loadBtn.onclick = loadPage
+  addBtn.onclick = initInSavePage
 }
 
 init();
