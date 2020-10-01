74d4751eff94f099e78d2497def30652bd11caf1
bohrzhang <bohrzhang@tencent.com>
2020-10-01 16:23:56 +0800

    feat: 添加信息管理页面

--- src/js/options.js
+++ src/js/options.js
@@ -2,23 +2,37 @@ const hostContainer = document.getElementById('host');
 const pathContainer = document.getElementById('path');
 const queryContainer = document.getElementById('query');
 
-chrome.storage.sync.get(['__wukongCache'], (result) => {
-  const { __wukongCache = {} } = result;
-  const { hostMap, pathMap, queryMap } = __wukongCache;
-  let hostHtml = '';
-  let pathHtml = '';
-  let queryHtml = '';
-  Object.keys(hostMap).forEach((hostKey) => {
-    hostHtml += `<li>${hostMap[hostKey]}: ${hostKey}</li>`;
+function init() {
+  chrome.storage.sync.get(['__wukongCache'], (result) => {
+    const { __wukongCache = {} } = result;
+    const { hostMap, pathMap, queryMap } = __wukongCache;
+    blockGen(hostMap, hostContainer)
+    blockGen(pathMap, pathContainer)
+    blockGen(queryMap, queryContainer)
   });
-  Object.keys(pathMap).forEach((pathKey) => {
-    pathHtml += `<li>${pathMap[pathKey]}: ${pathKey}</li>`;
-  });
-  Object.keys(queryMap).forEach((querykey) => {
-    queryHtml += `<li>${queryMap[querykey]}: ${querykey}</li>`;
-  });
+}
 
-  hostContainer.innerHTML = hostHtml;
-  pathContainer.innerHTML = pathHtml;
-  queryContainer.innerHTML = queryHtml;
-});
+// 根据数据生成对应区块
+function blockGen(data, container) {
+  let tbody = ''
+  const thead = `<thead><tr><th>名称</th><th>值</th><th>操作</th></tr></thead>`
+  Object.keys(data).forEach((key) => {
+    tbody += `<tr>
+      <td>${data[key]}</td>
+      <td>${key}</td>
+      <td class="delete">删除</td>
+    </tr>`;
+  });
+  tbody = `<tbody>${tbody}</tbody>`
+  container.append(str2cell(thead))
+  container.append(str2cell(tbody))
+}
+
+// 字符串转dom
+function str2cell(str) {
+  const table = document.createElement('table')
+  table.innerHTML = str
+  return table.firstChild
+}
+
+init()
